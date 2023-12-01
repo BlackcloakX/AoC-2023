9ed2c6e8d1a2a6a47185c965997dafbcc10de4fe
Michael <122835866+BlackcloakX@users.noreply.github.com>
2023-12-01 19:06:10 +0100

    made Day1-solution more succinct

--- src/Day01.kt
+++ src/Day01.kt
@@ -12,29 +12,18 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        val numbers = listOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+        val numbers = setOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+                             "0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
         var result = 0
         for(s in input){
             val first = s.findAnyOf(numbers)
             val last = s.findLastAnyOf(numbers)
-            val first_n = s.indexOfFirst { c -> c.isDigit() }
-            val last_n = s.indexOfLast { c -> c.isDigit() }
 
-            val num1: String = if(first != null && first.first < first_n) {
-                getNum(first.second)
-            } else{
-                s.first { c -> c.isDigit() }.toString()
-            }
-
-            val num2: String = if(last != null && last.first > last_n){
-                getNum(last.second)
-            } else{
-                s.last { c -> c.isDigit() }.toString()
-            }
+            val num1: String = if(first != null) getNum(first.second) else ""
+            val num2: String = if(last != null) getNum(last.second) else ""
 
             result += num1.toInt() * 10 + num2.toInt()
         }
-
         return result
     }
 
@@ -60,6 +49,6 @@ fun getNum(s: String): String{
         "seven" -> "7"
         "eight" -> "8"
         "nine" -> "9"
-        else -> {throw IllegalArgumentException()}
+        else -> s
     }
 }
