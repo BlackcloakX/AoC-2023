e5c91e38c6e19f8cd5e81cedbf75bcbfa74f29aa
Michael <122835866+BlackcloakX@users.noreply.github.com>
2023-12-09 12:21:40 +0100

    Day09 restructured

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,24 +1,11 @@
 fun main() {
     fun part1(input: List<String>): Long {
-        var sum = 0L
-        for(line in input){
-            val nums = parseLine(line)
-            sum += recursion(nums)
-        }
-
-        return sum
+        return getSum(input) { nums: List<Long> -> recursion(nums) }
     }
 
     fun part2(input: List<String>): Long {
-        var sum = 0L
-        for(line in input){
-            val nums = parseLine(line)
-            sum += recursion2(nums)
-        }
-
-        return sum
+        return getSum(input) { nums: List<Long> -> recursion2(nums) }
     }
-
 
     val input = part1(readInput("../resources/Day09"))
     val input2 = part2(readInput("../resources/Day09"))
@@ -26,8 +13,30 @@ fun main() {
     println(input2)
 }
 
+fun getSum(input:List<String>, function: (nums: List<Long>) -> Long): Long{
+    var sum = 0L
+    for(line in input){
+        val nums = parseLine(line)
+        sum += function(nums)
+    }
+
+    return sum
+}
+
 fun parseLine(input: String): List<Long>{
     return input.split(" ").map { e -> e.toLong() }
+}
+
+fun recursion(input: List<Long>): Long{
+    if(isAllZeroes(input)) return 0
+
+    return recursion(getDif(input)) + input[input.size - 1]
+}
+
+fun recursion2(input: List<Long>): Long{
+    if(isAllZeroes(input)) return 0
+
+    return input[0] - recursion2(getDif(input))
 }
 
 fun getDif(input: List<Long>): List<Long>{
@@ -45,16 +54,4 @@ fun isAllZeroes(input: List<Long>): Boolean{
         if(n != 0L) return false
     }
     return true
-}
-
-fun recursion(input: List<Long>): Long{
-    if(isAllZeroes(input)) return 0
-
-    return recursion(getDif(input)) + input[input.size - 1]
-}
-
-fun recursion2(input: List<Long>): Long{
-    if(isAllZeroes(input)) return 0
-
-    return input[0] - recursion2(getDif(input))
 }
